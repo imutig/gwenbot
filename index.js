4054685ab44f74d642c8f3ed30b91af6acdb8475
GwenBot <admin@xsgwen.fr>
2026-02-13 16:09:55 +0100

    chore: Remove trailing whitespace from a comment.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const TWITCH_CLIENT_SECRET = process.env.TWITCH_CLIENT_SECRET;
 const TWITCH_CHANNEL = process.env.TWITCH_CHANNEL?.replace('#', '') || 'xsgwen';
 const TWITCH_BOT_USERNAME = process.env.TWITCH_USERNAME || 'gwenbot';
 
-// Will be set after initialization
+// Will be set after initialization 
 let TWITCH_BOT_USER_ID = process.env.TWITCH_BOT_USER_ID;
 let TWITCH_BROADCASTER_ID = process.env.TWITCH_BROADCASTER_ID;
 
